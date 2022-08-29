5f0c1b0580740f771c1a9bf3dcf0259ea1683db6
qmli <leeqiming@163.com>
2022-08-29 11:42:37 +0800

    add file

--- test/shuffle.test.ts
+++ test/shuffle.test.ts
@@ -1,4 +1,12 @@
+import shuffle from "../src/shuffle";
+
 test('temp jest', () => {
     console.log('temp jest');
     expect(1).toEqual(1);
+})
+
+describe('shuffle', () => {
+    test('shuffle1', () => {
+        expect(shuffle([2, 5, 1, 3, 4, 7], 3)).toEqual([2, 3, 5, 4, 1, 7]);
+    })
 })
