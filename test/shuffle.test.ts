783e78a2e14c4891ef3d6f883cd925ff2e9aad60
qmli <leeqiming@163.com>
2022-08-29 11:56:40 +0800

    change desc

--- test/shuffle.test.ts
+++ test/shuffle.test.ts
@@ -6,15 +6,15 @@ test('temp jest', () => {
 })
 
 describe('shuffle', () => {
-    test('shuffle1', () => {
+    test('shuffle case 1', () => {
         expect(shuffle([2, 5, 1, 3, 4, 7], 3)).toEqual([2, 3, 5, 4, 1, 7]);
     })
 
-    test('shuffle2', () => {
+    test('shuffle case 2', () => {
         expect(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4)).toEqual([1, 4, 2, 3, 3, 2, 4, 1]);
     })
 
-    test('shuffle3', () => {
+    test('shuffle case 3', () => {
         expect(shuffle([1, 1, 2, 2], 2)).toEqual([1, 2, 1, 2]);
     })
 })
