d040601636bcbc49333b3a4b688b4eba977fba6e
qmli <leeqiming@163.com>
2022-08-29 11:10:53 +0800

    init

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import shuffle from "./shuffle";
+
+const numbers = shuffle([1, 2, 3, 4, 5], 4);
+
+console.log(numbers);
---
+++ src/shuffle.ts
@@ -0,0 +1,5 @@
+function shuffle(nums: number[], n: number): number[] {
+    return [];
+};
+
+export default shuffle;
