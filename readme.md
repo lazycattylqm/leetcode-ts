49fb15dabca2eb0da40d51c327fb33371ba6162f
LiQiming <leeqiming@163.com>
2022-08-31 17:13:29 +0800

    clean up code

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # leetcode-ts
+
 leetcode-ts
--- test/permuteUnique.test.ts
+++ test/permuteUnique.test.ts
@@ -1,13 +1,13 @@
 import permuteUnique from "../src/permuteUnique";
 
-describe('test permuteUnique', ()=>{
-    test('case 1', ()=>{
-        let unique = permuteUnique([1,1,2]);
+describe('test permuteUnique', () => {
+    test('case 1', () => {
+        let unique = permuteUnique([1, 1, 2]);
         console.log(unique)
     })
 
-    test('case 2', ()=>{
-        let unique = permuteUnique([1,2,3]);
+    test('case 2', () => {
+        let unique = permuteUnique([1, 2, 3]);
         console.log(unique)
     })
 })
