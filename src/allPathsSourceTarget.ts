cfc6a7c6c927ab4cb27407d0f3c1cc1387cb306a
LiQiming <leeqiming@163.com>
2022-09-01 18:38:36 +0800

    format code

--- src/allPathsSourceTarget.ts
+++ src/allPathsSourceTarget.ts
@@ -1,19 +1,19 @@
-function allPathsSourceTarget ( graph: number[][] ): number[][] {
+function allPathsSourceTarget (graph: number[][]): number[][] {
   let result: number[][] = [];
   let path: number[] = [0];
   const gLen = graph.length;
-  const dfs = ( node: number ): void => {
-    if ( node == gLen - 1 ) {
+  const dfs = (node: number): void => {
+    if (node == gLen - 1) {
       result = [...result, [...path]];
       return
     }
-    for ( const ele of graph[node] ) {
-      path.push( ele )
-      dfs( ele )
+    for (const ele of graph[node]) {
+      path.push(ele)
+      dfs(ele)
       path.pop()
     }
   }
-  dfs( 0 )
+  dfs(0)
   return result;
 }
 
