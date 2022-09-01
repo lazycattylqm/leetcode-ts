08f24a1650916e38d79c2ff0819a36a4d125ce5c
LiQiming <leeqiming@163.com>
2022-09-01 18:33:28 +0800

    797. 所有可能的路径

---
+++ src/allPathsSourceTarget.ts
@@ -0,0 +1,20 @@
+function allPathsSourceTarget ( graph: number[][] ): number[][] {
+  let result: number[][] = [];
+  let path: number[] = [0];
+  const gLen = graph.length;
+  const dfs = ( node: number ): void => {
+    if ( node == gLen - 1 ) {
+      result = [...result, [...path]];
+      return
+    }
+    for ( const ele of graph[node] ) {
+      path.push( ele )
+      dfs( ele )
+      path.pop()
+    }
+  }
+  dfs( 0 )
+  return result;
+}
+
+export default allPathsSourceTarget;
