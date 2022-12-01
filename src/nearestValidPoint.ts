75fa506c14c483718d024eccc892c2fde85731db
qmli <leeqiming@163.com>
2022-12-01 11:42:33 +0800

    1779. 找到最近的有相同 X 或 Y 坐标的点

--- src/nearestValidPoint.ts
+++ src/nearestValidPoint.ts
@@ -1,11 +1,11 @@
 export default function nearestValidPoint(x: number, y: number, points: number[][]): number {
     let distance = Number.MAX_VALUE
     let index = -1;
-    const valid= (x: number, y: number, points: number[]):boolean => {
+    const valid = (x: number, y: number, points: number[]): boolean => {
         return points[0] === x || points[1] === y;
     }
 
-    const dist = (x: number, y: number, points: number[]):number => {
+    const dist = (x: number, y: number, points: number[]): number => {
         return Math.abs(x - points[0]) + Math.abs(y - points[1]);
     }
     points.forEach((point, i) => {
